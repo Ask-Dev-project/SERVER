9492d65e25b0e26b143a0727a258b10270bdae4d
arulabdulaziz <rulz.matrixs@gmail.com>
2021-01-22 11:41:29 +0700

    init sequelize relation

---
+++ models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Post, {foreignKey: 'UserId'})
+      User.hasMany(models.Answer, {foreignKey: 'UserId'})
+    }
+  };
+  User.init({
+    email: DataTypes.STRING,
+    status: DataTypes.STRING,
+    nickname: DataTypes.STRING,
+    rating: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
