e21c66e32d29c048c376bf2cda192607a26176f7
arulabdulaziz <rulz.matrixs@gmail.com>
2021-01-23 22:04:42 +0700

    initia;l github oauth

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,8 @@ const UserController = require('../controllers/userController')
 const authentication = require('../midddlewares/authentication')
 
 route.post('/googleLogin', UserController.googleLogin)
+route.get('/githubLogin', UserController.toGitHubLogin)
+route.get('/oauth-callback', UserController.callBack)
 route.use(authentication)
 route.patch('/change-nickname', UserController.changeNickname)
 
