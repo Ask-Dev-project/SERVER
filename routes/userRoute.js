df75d24c1169319234221976c178c1f0a316164f
Andrenoj11 <andrenojuanda94@gmail.com>
2021-01-25 23:31:19 +0700

    add tdd coverage till 90 percent

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
 const route = require('express').Router()
 const UserController = require('../controllers/userController')
 const authentication = require('../midddlewares/authentication')
-route.post('/googleLogin', UserController.googleLogin)
+// route.post('/googleLogin', UserController.googleLogin)
 
-route.get('/githubLogin', UserController.toGitHubLogin)
-route.get('/oauth-callback', UserController.callBack)
+// route.get('/githubLogin', UserController.toGitHubLogin)
+// route.get('/oauth-callback', UserController.callBack)
 
 route.use(authentication)
 route.patch('/change-nickname', UserController.changeNickname)
